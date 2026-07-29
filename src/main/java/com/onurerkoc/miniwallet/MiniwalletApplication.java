f42e861a829d396a6b94d4a906e59c0114dca24d
onur erkoç <onurbenena@gmail.com>
2026-07-29 22:18:30 +0300

    başlangıç: MiniWallet Spring Boot projesini oluştur

---
+++ src/main/java/com/onurerkoc/miniwallet/MiniwalletApplication.java
@@ -0,0 +1,13 @@
+package com.onurerkoc.miniwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniwalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniwalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onurerkoc/miniwallet/MiniwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurerkoc.miniwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniwalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
